691e47d608785ecfeda4600f2ad2bb3255649e7f
EmilioRCPantoja <emiliorafael.pantoja02@gmail.com>
2025-03-16 20:27:56 -0300

    bubblesort com 10 numeros

--- ordem10.c
+++ ordem10.c
@@ -1,33 +1,35 @@
 #include<stdio.h>
 
-int main(){
-    int i=0, troca=0;
+int main()
+{
+    int i=0, troca=1, a=0;
     int num[10];
-
-    for (i=0;i<10;i++){
+    for(i=0;i<10;i++)
+    {
         printf("digite um numero: ");
         int n;
-        scanf("%i", &n);
+        scanf("%d", &n);
         num[i]=n;
-    }
-    if(num[i]>num[i+1]){
-        do
-    {
-            troca=num[i];
-            num[i]=num[i+1];
-            num[i+1]=troca;
-            troca=0;
-    }   while (troca==1);
 
-            for ( i = 0; i < 10; i++)
-            {
-            printf(" a ordem e:\n [%i]\n", num[i] );
-            }
-}   else
-    {
-    for ( i = 0; i < 10; i++)
-    {
-        printf(" a ordem e:\n [%i]\n", num[i] );
     }
-}
+    while(troca==1)
+    {
+        troca=0;
+        for(i=0;i<10;i++)
+        {
+            if(num[i]>num[i+1])
+            {
+                troca=1;
+                a=num[i];
+                num[i]=num[i+1];
+                num[i+1]=a;
+            }
+        }
+
+    }
+    printf(" a ordem sera:");
+    for(i=0;i<10;i++)
+    {
+        printf("\n%i", num[i]);
+    }
 }
